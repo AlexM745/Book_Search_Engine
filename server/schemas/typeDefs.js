002afb006f6d8093581b1e9340e544b5b3229e06
Alexis Merino <alexismerino49@gmail.com>
2024-06-19 04:41:06 -0500

    added type Book

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,6 +7,15 @@ email: String
 bookCount: Int
 savedBooks: [Book]
 }
+
+type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+}
 `;
 
 module.exports = typeDefs;
