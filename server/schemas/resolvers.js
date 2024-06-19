dbfa5258e43dc6fadc39b40d859336a0c112b992
Alexis Merino <alexismerino49@gmail.com>
2024-06-19 05:31:55 -0500

    added addUser to mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,7 +19,12 @@ const resolvers = {
     },
 
     Mutation: {
+        addUser: async (parent,{ username, email, password }) => {
+            const user = await User.create( {username, email, password });
+            const token = signToken(user);
 
+            return { token, user };
+        },
     },
 
 };
