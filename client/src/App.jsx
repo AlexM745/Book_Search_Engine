226666495e57bb2aab30baccfffbcf890d954fc0
Alexis Merino <alexismerino49@gmail.com>
2024-06-20 15:09:40 -0500

    added Apollo provider and GraphQL APi endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,21 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// importing Apollo provider
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+
 function App() {
   return (
     <>
