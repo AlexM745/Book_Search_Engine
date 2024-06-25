231bac25d6af4b0077cb3ac02975a738473e1d71
Alexis Merino <alexismerino49@gmail.com>
2024-06-24 22:14:46 -0500

    changed the authmiddleware and vite.config

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -12,10 +12,9 @@ module.exports = {
     },
   }),
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
+    let token = req.body.token || req.query.token || req.headers.authorization;
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
@@ -35,7 +34,7 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
